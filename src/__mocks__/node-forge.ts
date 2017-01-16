28ba8da473deafb338e29996a14e8d8669b49d72
Francois Ward <fward@phoenixmatrix.com>
2017-01-16 18:24:52 -0500

    some more test stuff

--- src/__mocks__/node-forge.ts
+++ src/__mocks__/node-forge.ts
@@ -1,3 +1,29 @@
+export const pemPrivateKeyFixture =
+`-----BEGIN RSA PRIVATE KEY-----
+MGICAQACEQCAovssw5Da4ASH7dFpwwzhAgMBAAECEALPjN11iHy/0FOsIH7T4GEC
+CQDTYSxRw2yKPwIJAJvKbc3H4WDfAghy+88iAw5N/QIIZ4IEmH+gH/MCCQCD17z+
+oB5vxw==
+-----END RSA PRIVATE KEY-----
+`;
+
+export const pemCertificateFixture =
+`-----BEGIN CERTIFICATE-----
+MIICUTCCAfugAwIBAgIBADANBgkqhkiG9w0BAQQFADBXMQswCQYDVQQGEwJDTjEL
+MAkGA1UECBMCUE4xCzAJBgNVBAcTAkNOMQswCQYDVQQKEwJPTjELMAkGA1UECxMC
+VU4xFDASBgNVBAMTC0hlcm9uZyBZYW5nMB4XDTA1MDcxNTIxMTk0N1oXDTA1MDgx
+NDIxMTk0N1owVzELMAkGA1UEBhMCQ04xCzAJBgNVBAgTAlBOMQswCQYDVQQHEwJD
+TjELMAkGA1UEChMCT04xCzAJBgNVBAsTAlVOMRQwEgYDVQQDEwtIZXJvbmcgWWFu
+ZzBcMA0GCSqGSIb3DQEBAQUAA0sAMEgCQQCp5hnG7ogBhtlynpOS21cBewKE/B7j
+V14qeyslnr26xZUsSVko36ZnhiaO/zbMOoRcKK9vEcgMtcLFuQTWDl3RAgMBAAGj
+gbEwga4wHQYDVR0OBBYEFFXI70krXeQDxZgbaCQoR4jUDncEMH8GA1UdIwR4MHaA
+FFXI70krXeQDxZgbaCQoR4jUDncEoVukWTBXMQswCQYDVQQGEwJDTjELMAkGA1UE
+CBMCUE4xCzAJBgNVBAcTAkNOMQswCQYDVQQKEwJPTjELMAkGA1UECxMCVU4xFDAS
+BgNVBAMTC0hlcm9uZyBZYW5nggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEE
+BQADQQA/ugzBrjjK9jcWnDVfGHlk3icNRq0oV7Ri32z/+HQX67aRfgZu7KWdI+Ju
+Wm7DCfrPNGVwFWUQOmsPue9rZBgO
+-----END CERTIFICATE-----
+`
+
 function generateKeyPair() {
   return {privateKey: {}, publicKey: {}};
 }
@@ -14,9 +40,9 @@ function createCertificate() {
   return mockCertificate;
 }
 
-const certificateToPem = jest.fn();
+const certificateToPem = jest.fn(() => pemCertificateFixture);
 
-const privateKeyToPem = jest.fn();
+const privateKeyToPem = jest.fn(() => pemPrivateKeyFixture);
 
 const rsa = {generateKeyPair};
 
--- src/__tests__/certificate.ts
+++ src/__tests__/certificate.ts
@@ -1,4 +1,8 @@
 import {generateKeyPair, createCertificateAuthority} from '../certificate';
+import * as forge from 'node-forge';
+
+const forgeFixtures = <any>forge;
+const {pki} = forge;
 
 describe('generateKeyPair', () => {
   it('creates a key pair', () => {
@@ -11,6 +15,12 @@ describe('createCertificateAuthority', () => {
   it('it generates a private key and certificate pair', () => {
     const keyPair = generateKeyPair();
 
-    const certificateAuthority = createCertificateAuthority(keyPair);
+    const ca = createCertificateAuthority(keyPair);
+    const spy = <jest.Mock<String>>pki.privateKeyToPem;
+    expect(spy.mock.calls.length).toBe(1);
+    expect(spy.mock.calls[0]).toEqual([keyPair.privateKey]);
+
+    expect(ca.privateKey).toBe(forgeFixtures.pemPrivateKeyFixture);
+    expect(ca.certificate).toBe(forgeFixtures.pemCertificateFixture);
   });
 });
